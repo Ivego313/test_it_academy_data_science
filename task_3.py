e91176d4d5abf4569a18404d4bead9b5f76348ea
Egor Invanov <ivego93@gmail.com>
2024-04-27 11:26:29 +0300

    Refactor task 3

--- task_3.py
+++ task_3.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 
 
-def get_days_date_difference(date_a: str, date_b: str, date_format: str = "%d/%m/%Y") -> int:
+def get_date_difference_days(date_a: str, date_b: str, date_format: str = "%d/%m/%Y") -> int:
     datetime_a = datetime.strptime(date_a, date_format)
     datetime_b = datetime.strptime(date_b, date_format)
     delta_time_days = (datetime_b - datetime_a).days
-    return abs(delta_time_days)
+    return delta_time_days
 
 
-a = "01/01/2023"
-b = "01/01/2024"
-days_difference = get_days_date_difference(a, b)
-print(days_difference)
+a = "01/01/2024"
+b = "01/01/2023"
+date_difference_days = get_date_difference_days(a, b)
+print(f"The difference between the dates {b} and {a} is {abs(date_difference_days)} days.")
