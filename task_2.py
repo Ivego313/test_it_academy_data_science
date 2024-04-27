4949b672c935018d7072b87e8e6890bff0a0f0b5
Egor Invanov <ivego93@gmail.com>
2024-04-27 11:13:21 +0300

    Add input checks in task 2

--- task_2.py
+++ task_2.py
@@ -2,13 +2,24 @@ def calculate_cosine_difference(vector_a: list, vector_b: list):
     if len(vector_a) != len(vector_b):
         print(f"Vectors have different dimensionality! Vector_a length: {len(vector_a)}, vector_b length: {len(vector_b)}.")
         return
-    if len(vector_a) == 0:
-        print("Vectors must be not zero vectors!")
+    if not vector_a:
+        print("Vectors are empty!")
         return
+    if is_zero_vector(vector_a):
+        print("Vector_a must be non-zero vector!")
+        return
+    if is_zero_vector(vector_b):
+        print("Vector_b must be non-zero vector!")
+        return
+
     dot_product = sum(vector_a[i] * vector_b[i] for i in range(len(vector_a)))
-    product_vector_lengths = get_vector_length(vector_a) * get_vector_length(vector_b)
-    cosine_difference = 1 - dot_product / product_vector_lengths
+    product_vectors_lengths = get_vector_length(vector_a) * get_vector_length(vector_b)
+    cosine_difference = 1 - dot_product / product_vectors_lengths
     print(f"Cosine difference is {round(cosine_difference, 5)}")
+
+
+def is_zero_vector(lst: list) -> bool:
+    return all(x == 0 for x in lst)
 
 
 def get_vector_length(vector: list) -> int:
